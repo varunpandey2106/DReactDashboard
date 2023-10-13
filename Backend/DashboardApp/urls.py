a9b79b1778f9d2a9c63882bedc14db2e4e70ee26
varunpandey2106 <varun.pandey2106@gmail.com>
2023-10-13 21:22:22 +0530

    react frontend added

--- Backend/DashboardApp/urls.py
+++ Backend/DashboardApp/urls.py
@@ -11,10 +11,10 @@ router.register(r'intensity', views.IntensitySectorDataView, basename='intensity
 router.register(r'country', views.CountryIntensityDataView, basename='countryintensitydata')#clear
 router.register(r'region', views.RegionIntensityDataView, basename='regionintensitydata' ) #clear
 router.register(r'source', views.SourceIntensityView, basename='sourceintensitydata') #clear
-router.register(r'topic', views.TopicRegionDataView, basename='topicregiondata') #review
+router.register(r'topic', views.TopicRegionDataView, basename='topicregiondata') #clear, manual count
 router.register(r'likelihood', views.LikelihoodYearDataView, basename='likelihoodyeardata') #clear 
 router.register(r'year', views.YearRelevanceDataView, basename='yearrelevance') #clear 
-router.register(r'end_year', views.EndYearRegionDataView, basename='endyearregion') #review/clear
+router.register(r'end_year', views.EndYearRegionDataView, basename='endyearregion') #review/clear: end year on y axis
 router.register(r'pestle', views.PestleLikelihoodDataView, basename='pestlelikelihood') #clear
 
 
